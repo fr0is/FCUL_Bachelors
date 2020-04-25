dd5c7c395900412d9ebb82a5a3b2f42eb164471b
fc51050 <34450544+fr0is@users.noreply.github.com>
2020-04-25 16:35:16 +0100

    Update Project

--- Projeto_de_Sistemas_de_Informação/Projeto/frontend/src/app/app.module.ts
+++ Projeto_de_Sistemas_de_Informação/Projeto/frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { DouroVinhasComponent } from './douro-vinhas/douro-vinhas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    DouroVinhasComponent
   ],
   imports: [
     BrowserModule,
--- Projeto_de_Sistemas_de_Informação/S3-Angular/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ Projeto_de_Sistemas_de_Informação/S3-Angular/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { PetsComponent }      from './pets/pets.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent },
